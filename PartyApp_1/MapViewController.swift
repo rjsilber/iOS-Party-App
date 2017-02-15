2f511b41dbc775ba8f4f60ba0233596f1d3ddbb5
Robyn Silber <rjsilber@gmail.com>
2017-02-14 23:54:15 -0500

    UI updates

--- PartyApp_1/MapViewController.swift
+++ PartyApp_1/MapViewController.swift
@@ -1,6 +1,19 @@
-import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
-
+    @IBOutlet var mapView: MKMapView!
+    
+    override func loadView() {
+        // create map view
+        mapView = MKMapView()
+        
+        // Set it as the view MapViewController
+        view = mapView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
 }
